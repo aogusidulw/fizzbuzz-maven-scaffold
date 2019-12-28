e1fb4bbe54a1e43e4b1c260cb74aad75f864efee
user <user@user-PC>
2019-12-28 15:28:46 +0800

    java-刘文-功能测试处 火星 红

---
+++ src/test/java/lw/Area.java
@@ -0,0 +1,32 @@
+package lw;
+
+public class Area {
+	private int X = 0;
+	private int Y = 0;
+	public Area(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+	}
+	public int getX() {
+		return X;
+	}
+	public void setX(int x) {
+		X = x;
+	}
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+	
+	public boolean containsPointWithX(int x) {
+		// TODO Auto-generated method stub
+		return (0 <= x) && (x <= X);
+	}
+	
+	public boolean containsPointWithY(int y) {
+		// TODO Auto-generated method stub
+		return (0 <= y) && (y <= Y);
+	}
+}
---
+++ src/test/java/lw/MarsRoverTest.java
@@ -0,0 +1,62 @@
+package lw;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class MarsRoverTest {
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	@Test
+	public void should_land_the_rover_on_area() throws Exception {
+		Area area = new Area(10, 10);
+		Rover rover = new Rover();
+		rover.land(area, 5, 5, "E");
+		String position = rover.getPosition();
+		assertThat(position).isEqualTo("55E");
+	}
+	
+	@Test
+	public void should_move_forward() throws Exception {
+		Area area = new Area(10, 10);
+		Rover rover = new Rover();
+		rover.land(area, 5, 5, "E");
+		rover.move();
+		String position = rover.getPosition();
+		assertThat(position).isEqualTo("65E");
+	}
+	
+	@Test
+	public void name() throws Exception {
+		Rover rover = new Rover();
+		RoverController roverController = new RoverController(rover);
+		String mission = "10,10,5,5,E,M,L,M,R";
+		String position = roverController.excute(mission);
+		assertThat(position).isEqualTo("66E");
+	}
+	
+	@Test
+	public void should_warning_when_land_out_of_area() throws Exception {
+		expectedException.expect(Exception.class);
+		expectedException.expectMessage("x=20 is out of area");
+		
+		Rover rover = new Rover();
+		RoverController roverController = new RoverController(rover);
+		String mission = "10,10,20,20,E,M,L,M,R";
+		roverController.excute(mission);
+	}
+	
+	@Test
+	public void should_warning_when_move_out_of_area() throws Exception {
+		expectedException.expect(Exception.class);
+		expectedException.expectMessage("不能移动到区域外");
+		
+		Rover rover = new Rover();
+		RoverController roverController = new RoverController(rover);
+		String mission = "10,10,9,9,E,M,M,M,M";
+		roverController.excute(mission);
+	}
+}
