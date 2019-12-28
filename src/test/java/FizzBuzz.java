42ee9cbaecce5cdb805a3c1d3aa18ea8f9bcef4d
user <user@user-PC>
2019-12-28 15:36:48 +0800

    把没用的删了

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -1,5 +1,5 @@
 public class FizzBuzz {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		for(int i = 1; i <= 100; i ++) {
 			String result = of(i);
@@ -7,15 +7,27 @@ public class FizzBuzz {
 		}
 	}
 	
-	public static  String of(int i) {
-		if(i%3 == 0) {
+	public static  String of(int i) throws Exception {
+		if( i <= 0) {
+			throw new Exception("Invalid input");
+		}else if(i%3 == 0) {
 			if(i%5 == 0) {
+				if(i%7 == 0) {
+					return "FizzBuzzWhizz";
+				}
 				return "FizzBuzz";
+			}else if(i%7 == 0){
+				return "FizzWhizz";
 			}else {
 				return "Fizz";
 			}
 		}else if(i%5 == 0) {
+			if(i%7 == 0){
+				return "BuzzWhizz";
+			}
 			return "Buzz";
+		}else if(i%7 == 0){
+			return "Whizz";
 		}else{
 			return String.valueOf(i) ;
 		}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,22 +1,35 @@
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 /*
  * ÁõÎÄ
  * */
 public class FizzBuzzTest {
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	@Test
-	public void should_return_Fizz_given_number_divisible_by_3() {
+	public void should_return_Fizz_given_number_divisible_le_0() throws Exception {
+		expectedException.expect(Exception.class);
+		expectedException.expectMessage("Invalid input");
+		  
+		FizzBuzz.of(0);	
+	}
+	
+	@Test
+	public void should_return_Fizz_given_number_divisible_by_3() throws Exception {
 		assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");	
 	}
 	
 	@Test
-	public void should_return_Buzz_given_number_divisible_by_5() {
+	public void should_return_Buzz_given_number_divisible_by_5() throws Exception {
 		assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
 	}
 	
 	@Test
-	public void should_FizzBuzz_Fizz_given_number_divisible_by_3and5() {
+	public void should_FizzBuzz_Fizz_given_number_divisible_by_3and5() throws Exception {
 		assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
 	}
 }
