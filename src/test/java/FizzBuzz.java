016de728a8911b134f0abf2acd9a81a3ca240086
user <user@user-PC>
2019-12-28 11:39:20 +0800

    java-刘文-功能测试处 红

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -9,9 +9,7 @@ public class FizzBuzz {
 	
 	public static  String of(int i) {
 		if(i%3 == 0) {
-			
-				return "Fizz";
-			
+			return "Fizz";
 		}else if(i%5 == 0) {
 			return "Buzz";
 		}else{
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,7 +1,20 @@
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import org.junit.Test;
-//liuwen
+
 public class FizzBuzzTest {
+	@Test
+	public void should_return_Fizz_given_number_divisible_by_3() {
+		assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");	
+	}
 	
+	@Test
+	public void should_return_Buzz_given_number_divisible_by_5() {
+		assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
+	}
+	
+	@Test
+	public void should_FizzBuzz_Fizz_given_number_divisible_by_3and5() {
+		assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
+	}
 }
